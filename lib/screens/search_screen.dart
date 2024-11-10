bdaf9e3d3f86a47d9347a893f3a8cfdb918cbcd6
chetanr250 <=>
2024-11-10 23:12:46 +0530

    Added error handling for image loading and internet connectivity, improved font family, and enhanced details screen.

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:quadb/screens/main_screen.dart';
 import 'package:quadb/screens/movie_grid.dart';
-import '../providers/providers.dart';
+import 'package:quadb/providers/providers.dart';
 
 class SearchScreen extends ConsumerWidget {
   const SearchScreen({Key? key}) : super(key: key);
